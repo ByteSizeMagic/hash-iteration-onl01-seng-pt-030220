047cb3393d0c826c7d94c415d72e28c4ebe589cc
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-13 06:47:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,6 +9,6 @@ def happy_birthday(birthday_kids)
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
-    end
+      puts "You are too old for this."
   end
 end
